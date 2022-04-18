bb55cc73aaf5c7319184cbe8b6303feadc1a424a
王洋 <1316130400@qq.com>
2022-04-18 18:36:28 +0800

    2-7 Add hello test.http

---
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -0,0 +1,40 @@
+package com.jiawa.wiki.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
+    /**
+     * GET, POST, PUT, DELETE
+     *
+     * /user?id=1
+     * /user/1
+     * @return
+     */
+    // @PostMapping
+    // @PutMapping
+    // @DeleteMapping
+    // @RequestMapping(value = "/user/1", method = RequestMethod.GET)
+    // @RequestMapping(value = "/user/1", method = RequestMethod.DELETE)
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
+    }
+
+}
